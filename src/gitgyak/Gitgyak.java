7ddddb6744d5da0d52200aec6a06abb1a250ed61
dkornel01 <dobrocsi.kornel@diak.szamalk-szalezi.hu>
2023-09-20 12:50:57 +0200

    ciklus

--- src/gitgyak/Gitgyak.java
+++ src/gitgyak/Gitgyak.java
@@ -1,20 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package gitgyak;
 
-/**
- *
- * @author DobrocsiKornél(SZF_2
- */
+
 public class Gitgyak {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        for (int i = 0; i < 10; i++) {
+          
+        }
     }
     
 }
